2b9572d2e4af631af4573cf5a8c2bcb0d57ae024
shaoxp <shaoxp@simplenature.cn>
2021-06-08 17:44:35 +0800

    add workspace

---
+++ add/add-one/src/lib.rs
@@ -0,0 +1,19 @@
+use rand;
+pub fn add_one(x: i32) -> i32{
+    x+1
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+
+    #[test]
+    fn test_add_one(){
+        assert_eq!(3,add_one(2));
+    }
+}
---
+++ add/adder/src/main.rs
@@ -0,0 +1,6 @@
+use add_one;
+use rand;
+
+fn main() {
+    println!("3 plus one is {}", add_one::add_one(3));
+}
