aa85be75bc0118003069c8efc07fae2188b26fec
shaoxp <shaoxp@simplenature.cn>
2021-06-08 16:06:36 +0800

    add crate manage

---
+++ my_crate/src/lib.rs
@@ -0,0 +1,21 @@
+//! # My Crate
+//! 
+//! `my_crate` is a collection of utilities to make performing certain
+//! calculations more convenient
+
+/// Add one to the number of given.
+/// # Examples
+/// ```
+/// let arg = 5;
+/// let answer = my_crate::add_one(arg);
+/// assert_eq!(6,answer);
+/// ```
+/// ```
+/// let arg = -1;
+/// let answer = my_crate::add_one(arg);
+/// assert_eq!(0,answer);
+pub fn add_one(x: i32) -> i32{
+    x+1
+}
+
+pub mod art;
