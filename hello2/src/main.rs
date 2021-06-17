3307bdbc0772c55833f8b0efcfac4bc279ebe492
shaoxp <shaoxp@simplenature.cn>
2021-06-17 16:30:40 +0800

    add another example for hello use crate from crates.io

--- hello2/src/main.rs
+++ hello2/src/main.rs
@@ -5,16 +5,16 @@ use std::fs;
 use std::thread;
 use std::time::Duration;
 
-use hello::ThreadPool;
+use scheduled_thread_pool::ScheduledThreadPool;
 
 fn main() {
     println!("Hello, world!");
 
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-    let pool = ThreadPool::new(4);
+    let pool = ScheduledThreadPool::with_name("thread :{} ",4);
 
     let mut n = 0;
-    for stream in listener.incoming().take(400) {
+    for stream in listener.incoming() {
         let stream = stream.unwrap();
 
         println!("Connection established! {}",n);
@@ -38,14 +38,12 @@ fn hanlde_request(mut stream: TcpStream){
     let sleep = b"GET /sleep HTTP/1.1\r\n";
     let crash = b"GET /crash HTTP/1.1\r\n";
     let (status_line,contentfile) = if buffer.starts_with(get.as_bytes()) {
-        ("HTTP/1.1 200 OK\r\n\r\n","res/hello.html")
+        ("HTTP/1.1 200 OK\r\n\r\n","hello.html")
     }else if buffer.starts_with(sleep){
         thread::sleep(Duration::from_secs(5));
-        ("HTTP/1.1 200 OK\r\n\r\n","res/hello.html")
+        ("HTTP/1.1 200 OK\r\n\r\n","hello.html")
     }else if buffer.starts_with(crash){
-        panic!("panic!", );
-        thread::sleep(Duration::from_secs(5));
-        ("HTTP/1.1 200 OK\r\n\r\n","res/hello.html")
+        panic!("abc");
     }else{
         ("HTTP/1.1 404 NOT FOUND\r\n\r\n","404.html")
     
