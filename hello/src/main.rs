2d15312b0c7250300e4a6dee968b0b0875bd050c
shaoxp <shaoxp@simplenature.cn>
2021-06-18 10:10:27 +0800

    add fmt

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,7 +1,7 @@
+use std::fs;
+use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
-use std::io::prelude::*;
-use std::fs;
 use std::thread;
 use std::time::Duration;
 
@@ -17,42 +17,40 @@ fn main() {
     for stream in listener.incoming().take(400) {
         let stream = stream.unwrap();
 
-        println!("Connection established! {}",n);
-        pool.execute(||{
+        println!("Connection established! {}", n);
+        pool.execute(|| {
             hanlde_request(stream);
         });
-       
-        n +=1;
+
+        n += 1;
     }
 
     println!("shutting down start!!");
 }
 
-fn hanlde_request(mut stream: TcpStream){
-
-    let mut buffer = [0;1024];
+fn hanlde_request(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
     stream.read(&mut buffer).unwrap();
     // println!("{}", String::from_utf8_lossy(&buffer));
 
     let get = "GET / HTTP/1.1\r\n";
     let sleep = b"GET /sleep HTTP/1.1\r\n";
     let crash = b"GET /crash HTTP/1.1\r\n";
-    let (status_line,contentfile) = if buffer.starts_with(get.as_bytes()) {
-        ("HTTP/1.1 200 OK\r\n\r\n","res/hello.html")
-    }else if buffer.starts_with(sleep){
+    let (status_line, contentfile) = if buffer.starts_with(get.as_bytes()) {
+        ("HTTP/1.1 200 OK\r\n\r\n", "res/hello.html")
+    } else if buffer.starts_with(sleep) {
         thread::sleep(Duration::from_secs(5));
-        ("HTTP/1.1 200 OK\r\n\r\n","res/hello.html")
-    }else if buffer.starts_with(crash){
-        panic!("panic!", );
+        ("HTTP/1.1 200 OK\r\n\r\n", "res/hello.html")
+    } else if buffer.starts_with(crash) {
+        panic!("panic!",);
         thread::sleep(Duration::from_secs(5));
-        ("HTTP/1.1 200 OK\r\n\r\n","res/hello.html")
-    }else{
-        ("HTTP/1.1 404 NOT FOUND\r\n\r\n","404.html")
-    
+        ("HTTP/1.1 200 OK\r\n\r\n", "res/hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
     };
 
     let contents = fs::read_to_string(contentfile).unwrap();
-    let response = format!("{}{}",status_line,contents);
+    let response = format!("{}{}", status_line, contents);
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
 }
--- hello2/src/main.rs
+++ hello2/src/main.rs
@@ -1,7 +1,7 @@
+use std::fs;
+use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
-use std::io::prelude::*;
-use std::fs;
 use std::thread;
 use std::time::Duration;
 
@@ -11,46 +11,44 @@ fn main() {
     println!("Hello, world!");
 
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-    let pool = ScheduledThreadPool::with_name("thread :{} ",4);
+    let pool = ScheduledThreadPool::with_name("thread :{} ", 4);
 
     let mut n = 0;
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        println!("Connection established! {}",n);
-        pool.execute(||{
+        println!("Connection established! {}", n);
+        pool.execute(|| {
             hanlde_request(stream);
         });
-       
-        n +=1;
+
+        n += 1;
     }
 
     println!("shutting down start!!");
 }
 
-fn hanlde_request(mut stream: TcpStream){
-
-    let mut buffer = [0;1024];
+fn hanlde_request(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
     stream.read(&mut buffer).unwrap();
     // println!("{}", String::from_utf8_lossy(&buffer));
 
     let get = "GET / HTTP/1.1\r\n";
     let sleep = b"GET /sleep HTTP/1.1\r\n";
     let crash = b"GET /crash HTTP/1.1\r\n";
-    let (status_line,contentfile) = if buffer.starts_with(get.as_bytes()) {
-        ("HTTP/1.1 200 OK\r\n\r\n","hello.html")
-    }else if buffer.starts_with(sleep){
+    let (status_line, contentfile) = if buffer.starts_with(get.as_bytes()) {
+        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
+    } else if buffer.starts_with(sleep) {
         thread::sleep(Duration::from_secs(5));
-        ("HTTP/1.1 200 OK\r\n\r\n","hello.html")
-    }else if buffer.starts_with(crash){
+        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
+    } else if buffer.starts_with(crash) {
         panic!("abc");
-    }else{
-        ("HTTP/1.1 404 NOT FOUND\r\n\r\n","404.html")
-    
+    } else {
+        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
     };
 
     let contents = fs::read_to_string(contentfile).unwrap();
-    let response = format!("{}{}",status_line,contents);
+    let response = format!("{}{}", status_line, contents);
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
 }
